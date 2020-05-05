70d9739f3811280506d25005c27e495073c83fd5
Vishwanatha JOIS <SESA287006@se.com>
2020-05-05 16:26:15 +0530

    first commit

---
+++ Script.js
@@ -0,0 +1,29 @@
+$(function() {
+    //----- OPEN
+    $('[data-popup-open]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-open');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+ 
+        e.preventDefault();
+    });
+ 
+    //----- CLOSE
+    $('[data-popup-close]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-close');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+ 
+        e.preventDefault();
+    });
+});
+
+function showpop(id)
+{
+    document.getElementById('id').style.display = 'block';
+    
+}
+
+function closepop(id)
+{
+    document.getElementById('id').style.display = 'none';
+    
+}
