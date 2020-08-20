d95e4559978a02756ad9be0a4d76d09e864ee8d2
Vishwanatha JOIS <SESA287006@se.com>
2020-08-20 14:03:14 +0530

    checking JSV2

---
+++ data_EN.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc1").attr("href", res);
+    document.getElementById("wdlc1").text = "Safety at Schneider Electric";
+});
