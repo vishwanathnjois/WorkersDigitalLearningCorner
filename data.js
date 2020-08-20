3b5e6a7058431acaf1e3a303075ea1d59f5cd6c0
Vishwanatha JOIS <SESA287006@se.com>
2020-08-20 14:36:18 +0530

    3rd Push

---
+++ data.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+    $("#wdlc1").attr("href", "/ui/lms-learner-search/search?query=GSCH%26SE0001038");
+
+    $("#wdlc2").attr("href", "/ui/lms-learner-search/search?query=GSCSAFE000003%20GSCH%26SE0001037");
+
+    $("#wdlc4").attr("href", "/ui/lms-learner-search/search?query=");
+
+    $("#wdlc5").attr("href", "/ui/lms-learner-search/search?query=");
+
+    $("#wdlc6").attr("href", "/ui/lms-learner-search/search?query=");
+
+    $("#wdlc7").attr("href", "/ui/lms-learner-search/search?query=");
+
+    $("#wdlc8").attr("href", "/ui/lms-learner-search/search?query=");
+
+    $("#wdlc9").attr("href", "/ui/lms-learner-search/search?query=");
+
+    $("#wdlc10").attr("href", "/ui/lms-learner-search/search?query=");
+
+    $("#wdlc11").attr("href", "/ui/lms-learner-search/search?query=");
+
+    $("#wdlc12").attr("href", "/ui/lms-learner-search/search?query=");
+
+    $("#wdlc13").attr("href", "/ui/lms-learner-search/search?query=");
+
+    $("#wdlc14").attr("href", "/ui/lms-learner-search/search?query=");
+
+    $("#wdlc15").attr("href", "/ui/lms-learner-search/search?query=");
+
+    $("#wdlc16").attr("href", "/ui/lms-learner-search/search?query=");
+
+    $("#wdlc17").attr("href", "/ui/lms-learner-search/search?query=");
+
+    $("#wdlc18").attr("href", "/ui/lms-learner-search/search?query=");
+
+    $("#wdlc19").attr("href", "/ui/lms-learner-search/search?query=");
+
+    $("#wdlc20").attr("href", "/ui/lms-learner-search/search?query=");
+
+    $("#wdlc21").attr("href", "/ui/lms-learner-search/search?query=");
+
+    $("#wdlc22").attr("href", "/ui/lms-learner-search/search?query=");
+
+    $("#wdlc23").attr("href", "/ui/lms-learner-search/search?query=");
+
+    $("#wdlc24").attr("href", "/ui/lms-learner-search/search?query=");
+
+    $("#wdlc25").attr("href", "/ui/lms-learner-search/search?query=");
+
+    $("#wdlc26").attr("href", "/ui/lms-learner-search/search?query=");
+
+    $("#wdlc27").attr("href", "/ui/lms-learner-search/search?query=");
+
+    $("#wdlc28").attr("href", "/ui/lms-learner-search/search?query=");
+
+    $("#wdlc29").attr("href", "/ui/lms-learner-search/search?query=");
+
+    $("#wdlc30").attr("href", "/ui/lms-learner-search/search?query=");
+    
+});
