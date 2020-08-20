1677adbb1cba0dffad7d0b27b7b30edb1b287ddf
Vishwanatha JOIS <SESA287006@se.com>
2020-08-20 14:25:02 +0530

    2nd Push

--- data_EN.js
+++ data_EN.js
@@ -2,4 +2,152 @@ $(document).ready(function () {
     var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
     $("#wdlc1").attr("href", res);
     document.getElementById("wdlc1").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCSAFE000003%20GSCH%26SE0001037";
+    $("#wdlc2").attr("href", res);
+    document.getElementById("wdlc2").text = "Electrical Safety";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc3").attr("href", res);
+    document.getElementById("wdlc3").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc4").attr("href", res);
+    document.getElementById("wdlc4").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc5").attr("href", res);
+    document.getElementById("wdlc5").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc6").attr("href", res);
+    document.getElementById("wdlc6").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc7").attr("href", res);
+    document.getElementById("wdlc7").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc8").attr("href", res);
+    document.getElementById("wdlc8").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc9").attr("href", res);
+    document.getElementById("wdlc9").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc10").attr("href", res);
+    document.getElementById("wdlc10").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc11").attr("href", res);
+    document.getElementById("wdlc11").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc12").attr("href", res);
+    document.getElementById("wdlc12").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc13").attr("href", res);
+    document.getElementById("wdlc13").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc14").attr("href", res);
+    document.getElementById("wdlc14").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc15").attr("href", res);
+    document.getElementById("wdlc15").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc16").attr("href", res);
+    document.getElementById("wdlc16").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc17").attr("href", res);
+    document.getElementById("wdlc17").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc18").attr("href", res);
+    document.getElementById("wdlc18").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc19").attr("href", res);
+    document.getElementById("wdlc19").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc20").attr("href", res);
+    document.getElementById("wdlc20").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc21").attr("href", res);
+    document.getElementById("wdlc21").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc22").attr("href", res);
+    document.getElementById("wdlc22").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc23").attr("href", res);
+    document.getElementById("wdlc23").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc24").attr("href", res);
+    document.getElementById("wdlc24").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc25").attr("href", res);
+    document.getElementById("wdlc25").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc26").attr("href", res);
+    document.getElementById("wdlc26").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc27").attr("href", res);
+    document.getElementById("wdlc27").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc28").attr("href", res);
+    document.getElementById("wdlc28").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc29").attr("href", res);
+    document.getElementById("wdlc29").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc30").attr("href", res);
+    document.getElementById("wdlc30").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc31").attr("href", res);
+    document.getElementById("wdlc31").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc32").attr("href", res);
+    document.getElementById("wdlc32").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc33").attr("href", res);
+    document.getElementById("wdlc33").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc34").attr("href", res);
+    document.getElementById("wdlc34").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc35").attr("href", res);
+    document.getElementById("wdlc35").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc36").attr("href", res);
+    document.getElementById("wdlc36").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc37").attr("href", res);
+    document.getElementById("wdlc37").text = "Safety at Schneider Electric";
+
+    var res = "/ui/lms-learner-search/search?query=GSCH%26SE0001038";
+    $("#wdlc38").attr("href", res);
+    document.getElementById("wdlc38").text = "Safety at Schneider Electric";
 });
