88f55f0f38fbdcfe22e6d7f90d925d12c42e328d
Vishwanatha JOIS <SESA287006@se.com>
2021-02-10 13:00:24 +0530

    Kannada js added

---
+++ data_Kannada.js
@@ -0,0 +1,79 @@
+$(document).ready(function () {
+    document.getElementById("wdlc1").text = "ಷ್ನೇಯ್ಡರ್ನಲ್ಲಿ ಸುರಕ್ಷತೆ";
+    document.getElementById("wdlc2").text = "Electrical Safety";
+    document.getElementById("wdlc3").text = "Ergonomics";
+    document.getElementById("wdlc4").text = "Start Safe";
+    document.getElementById("wdlc5").text = "Other Topics";
+    document.getElementById("wdlc6").text = "COVID-19: What You Need to Know";
+    document.getElementById("wdlc7").text = "Managing Your Well-Being During Covid 19";
+
+
+    document.getElementById("wdlc11").text = "Core Values";
+    document.getElementById("wdlc12").text = "The Innovation Circle: 3 Phases of Innovation & Change";
+    document.getElementById("wdlc13").text = "40 Principles of SPS";
+    document.getElementById("wdlc14").text = "Our Company Story";
+    document.getElementById("wdlc15").text = "EcoStruxure";
+    document.getElementById("wdlc16").text = "STRIVE";
+    document.getElementById("wdlc17").text = "SSI";
+
+
+    document.getElementById("wdlc21").text = "Customer First";
+    document.getElementById("wdlc22").text = "Persona";
+
+
+    document.getElementById("wdlc31").text = "Warehouse Management Awareness";
+    document.getElementById("wdlc32").text = "Smart DC";
+    document.getElementById("wdlc33").text = "Transport";
+
+
+    document.getElementById("wdlc41").text = "Lean Manufacturing";
+    document.getElementById("wdlc42").text = "Short Interval Management";
+    document.getElementById("wdlc43").text = "Smart Factory";
+    document.getElementById("wdlc44").text = "Supply Chain Management";
+
+
+
+    document.getElementById("wdlc51").text = "Optimize ETO Processes";
+
+
+
+    document.getElementById("wdlc61").text = "Customer Satisfaction & Quality";
+    document.getElementById("wdlc62").text = "Health & Safety";
+    document.getElementById("wdlc63").text = "Industrialization";
+    document.getElementById("wdlc64").text = "Logistics";
+    document.getElementById("wdlc65").text = "Manufacturing";
+    document.getElementById("wdlc66").text = "All";
+
+
+    document.getElementById("wdlc71").text = "Together, We Grow!";
+    document.getElementById("wdlc72").text = "Communication & I";
+    document.getElementById("wdlc73").text = "Change Avengers";
+    document.getElementById("wdlc74").text = "Secrets of Happiness";
+    document.getElementById("wdlc75").text = "Entrepreneur Starting Kit";
+
+
+    document.getElementById("wdlc81").text = "goFluent";
+    document.getElementById("wdlc82").text = "Learn English";
+    document.getElementById("wdlc83").text = "Learn Spanish";
+    document.getElementById("wdlc84").text = "Learn French";
+
+
+
+    document.getElementById("wdlc91").text = "Microsoft Office 365";
+    document.getElementById("wdlc95").text = "Computer Skills";
+    document.getElementById("wdlc96").text = "Digital Capability";
+    document.getElementById("wdlc97").text = "Computer Basics";
+
+
+
+    document.getElementById("wdlc101").text = "My LearningLink";
+    document.getElementById("wdlc102").text = "Browse the Catalog";
+    document.getElementById("wdlc103").text = "Schneider IQ";
+    document.getElementById("wdlc104").text = "GSC Academy";
+    document.getElementById("wdlc105").text = "Employee Development Portal";
+    document.getElementById("wdlc106").text = "Cross-Functional Learning Corner";
+    document.getElementById("wdlc107").text = "Basic Electrical Theory";
+    document.getElementById("wdlc108").text = "External Digital Trainings";
+
+
+});
