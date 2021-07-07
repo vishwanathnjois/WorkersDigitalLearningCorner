8996b16ecd6ac35622b16944340f8e9a38cdfc8e
2011-harshitaroy-se <harshita.roy.se@gmail.com>
2021-07-07 13:51:30 +0530

    sse 2h in arabic

--- data_Arabic.js
+++ data_Arabic.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
 
     document.getElementById("wdlc11").text = "Schneider Essentials";
     document.getElementById("wdlc12").text = "EcoStruxure";
-    document.getElementById("wdlc13").text = "SSE";
+    document.getElementById("wdlc13").text = "SSE 2h";
     document.getElementById("wdlc14").text = "Lean Manufacturing";
     document.getElementById("wdlc15").text = "Quality";
     document.getElementById("wdlc16").text = "Logistics";
