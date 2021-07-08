14437ce7c3138403dac91b03cbba888c20fc2af3
2011-harshitaroy-se <harshita.roy.se@gmail.com>
2021-07-08 13:12:24 +0530

    french (strive)

--- data_french.js
+++ data_french.js
@@ -80,6 +80,7 @@ $(document).ready(function () {
     document.getElementById("wdlc107").text = "Connaissances de base sur  l'électricité ";
     document.getElementById("wdlc108").text = "Formations digitales externes";
 
-	
+	document.getElementById("wdlc109").text = "Strive";
+
     
 });
