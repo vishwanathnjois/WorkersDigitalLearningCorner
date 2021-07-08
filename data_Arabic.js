a18851023ac53fbf374f26973e88a6390e0164c0
2011-harshitaroy-se <harshita.roy.se@gmail.com>
2021-07-08 13:23:53 +0530

    Update data_Arabic.js

--- data_Arabic.js
+++ data_Arabic.js
@@ -78,6 +78,10 @@ $(document).ready(function () {
     document.getElementById("wdlc107").text = "زاوية التعلم للوظائف التبادلية";
     document.getElementById("wdlc108").text = "التدريب الرقمي الخارجي";
 	
+	document.getElementById("wdlc109").text = "Strive";
+	
+	
+	
 
 
     
